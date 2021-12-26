8ccf14f66d39f47b1235d56c6720465812cbc968
sergi <!Redevill12>
2021-12-26 23:29:53 +0000

    Add items:  - multi drawables
    Updated items:
    - multiple activity's
    - fragment_settings.xml
    - styles.xml
    - dimens.xml
    - fragment_settings.xml
    - colors.xml
    
    notes:
    - ...

--- app/src/main/java/ipca/am2/projeto2122/friendschat/MainActivity.kt
+++ app/src/main/java/ipca/am2/projeto2122/friendschat/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
--- app/src/main/java/ipca/am2/projeto2122/friendschat/ui/intro/WelcomeActivity.kt
+++ app/src/main/java/ipca/am2/projeto2122/friendschat/ui/intro/WelcomeActivity.kt
@@ -8,5 +8,9 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        supportActionBar?.hide()
+
+
     }
 }
