3490c7acb646a91947bf614128b1a23f3ffad4e9
sergi <!Redevill12>
2021-12-28 17:30:53 +0000

    Add items:  - multi drawables  - MessageChatActivity.kt  - message_right_rowview.xml
    Updated items:
    - updated multi layouts
    - colors.xml
    
    notes:
    - ...

---
+++ app/src/main/java/ipca/am2/projeto2122/friendschat/ui/MessageChatActivity.kt
@@ -0,0 +1,12 @@
+package ipca.am2.projeto2122.friendschat.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ipca.am2.projeto2122.friendschat.R
+
+class MessageChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message_chat)
+    }
+}
