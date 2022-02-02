65dcdbd7ffa0c0d26905996b538959db3203c401
sergio <a18074@alunos.ipca.pt>
2022-02-02 03:13:47 +0000

    Work Done: Add items: - ic_log_out.xml - main_activity_nav_menu.xml
    Updated items:
    - MainActivity.kt
    
    notes:
    - Bugs on the code are resolved!!

--- app/src/main/java/ipca/am2/projeto2122/friendschat/MainActivity.kt
+++ app/src/main/java/ipca/am2/projeto2122/friendschat/MainActivity.kt
@@ -1,6 +1,10 @@
 package ipca.am2.projeto2122.friendschat
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
@@ -9,7 +13,11 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import ipca.am2.projeto2122.friendschat.databinding.ActivityMainBinding
+import ipca.am2.projeto2122.friendschat.ui.intro.WelcomeActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -42,5 +50,34 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_activity_nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId)
+        {
+            R.id.action_sign_out ->
+            {
+                FirebaseAuth.getInstance().signOut()
+
+                val intent = Intent(this@MainActivity, WelcomeActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+
+                return true
+            }
+        }
+        return false
+    }
+
 }
